4bee78368763feaec6ac598f3a19eae2bb512591
Alun Owen <alunhowen@gmail.com>
2024-11-11 17:43:14 +0000

    Astro Tutorial: Random Domain did not work, reverting back

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://<random-string>.pages.github.io'
+    site: 'https://wefan-da.github.io'
 })
